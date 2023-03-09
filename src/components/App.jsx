1b0912706d5ca5f6cd5bc1a5a23b7f67a288beea
Hatrison <vadimulanovskiy@gmail.com>
2023-03-09 22:42:54 +0200

    Added feature for usage local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,32 @@ import ContactList from './ContactList';
 import { Header, SectionHeader } from './App.styled';
 import Filter from './Filter';
 
+const LS_KEY = 'contacts';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem(LS_KEY);
+
+    if (contacts !== null) {
+      this.setState({
+        contacts: JSON.parse(contacts),
+      });
+      return;
+    }
+
+    this.setState({ contacts: [] });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   onSubmit = values => {
     if (this.state.contacts.find(contact => contact.name === values.name)) {
